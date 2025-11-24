785fbae717587c138b4ce86871d94ad2d18db589
HaffordD <drew.hafford@maine.edu>
2025-11-24 12:25:59 -0500

    Made a working hash

--- hashtable.py
+++ hashtable.py
@@ -2,6 +2,7 @@
 #11/20
 #Got help from Caramon
 import csv
+import time
  
 
 class DataItem:
@@ -18,17 +19,21 @@ class DataItem:
 
 class hashTable:
     def __init__(self,size):
-        self.hashTable = [0]*size
+        self.hashTable = [None]*size
         self.capacity = size
         self.curSize = 0
+        self.collisions = 0
 
     def addByQuote(self, data):
         index = self.hashing_it(data[8])
+        print(data[8])
         while self.hashTable[index] != None: #collision
             index += 1
             if index > self.capacity:
                 index = 0
+            self.collisions += 1
         self.hashTable[index] = data
+        self.curSize += 1
         return
 
 
@@ -36,9 +41,11 @@ class hashTable:
         index = self.hashing_it(data[0])
         while self.hashTable[index] != None: #collision
             index += 1
-            if index > self.capacity:
+            if index >= self.capacity:
                 index = 0
+            self.collisions += 1
         self.hashTable[index] = data
+        self.curSize += 1
         return
 
 
@@ -54,14 +61,36 @@ def main():
     file = "MOCK_DATA.csv"
     quoteHash = hashTable(size)
     nameHash = hashTable(size)
+
+    #FOR THE TITLES
+    start = time.time()
+    rowCount = 0
     with open(file, 'r', newline = '', encoding="utf8") as csvfille:
         reader = csv.reader(csvfille)
         for row in reader:
-            print(row)
-            counter+=1
-        
+            if rowCount == 0:
+                rowCount += 1
+            else:
+                nameHash.addByName(row)
+    end = time.time()
+    print(f"There were {nameHash.collisions} collisions")#number of collisions
+    print(f"It took {end-start:0.2f} seconds to sort by Title")#time it took
+    print(f"There were {size-nameHash.curSize} wasted buckets")#wasted buckets
+    rowCount = 0
+   #FOR THE QUOTES
+    start = time.time()
+    with open(file, 'r', newline = '', encoding="utf8") as csvfille:
+        reader = csv.reader(csvfille)
+        for row in reader:
+            if rowCount == 0:
+                rowCount += 1
+            else:
+                quoteHash.addByQuote(row)
+    end = time.time()
 
-    print(counter)
+    print(f"There were {nameHash.collisions} collisions")#number of collisions
+    print(f"It took {end-start:0.2f} seconds to sort by quote")#time it took
+    print(f"There were {size-nameHash.curSize} wasted buckets")#wasted buckets
 
 if __name__ == "__main__":
     main()
